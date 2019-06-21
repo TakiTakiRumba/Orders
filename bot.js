198983b72cfc8c96fe725935b5d413e4caa2edee
TakiTakiRumba <52072176+TakiTakiRumba@users.noreply.github.com>
2019-06-21 13:57:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,44 +1,45 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = ".";
+var prefix = "£";
 
-client.on('ready', () => {
-	console.log('Dream iS Here');
-});
+client.on("message",async message => {
+  if(message.content === '£order'){//الامر
+  var shopc = message.guild.channels.find("name","orders")
+    if(!shopc) return message.reply("There is no room have the name orders")
+      let shop = '';
+        let fillter = m => m.author.id === message.author.id
+        
+       
+  
+        await message.channel.send("** ☞  Hello Sir , Write Your Order Please ☺ . **").then(e => {
+             message.channel.awaitMessages(fillter, { time: 60000, max: 1                                    
+  })
+       .then(co => {
+         shop = co.first().content;
+          co.first().delete();
+       // 90 + 40 + 20 + 20 + 10 + 20 = 200
+  let desc = '';
+  let nord = new Discord.RichEmbed()
+  .setColor("#b42323")
+  .addField("New Order : ", shop)
+  .setTimestamp()
+   e.edit("**Your Order Was Sended , Thanks Sir . ☺**").then(e => {
+    shopc.send('**That Order Sent By : ☞ **' + message.author)
+    shopc.sendEmbed(nord)
+    
+    })
+  })
+    })
+  }
+             
+        
+    
+       
+    
+        
+       
+  }
+  );
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`b 1.0`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-});
 
-
-
-    const dreamv2 = ["589506382211055616"]
-    client.on('message', message => {
-        var dream = message.content.split(` `).slice(1).join(' ');
-          if (!dreamv2.includes(message.author.id)) return;
-          if(message.content.startsWith(prefix + "ply")){
-            client.user.setActivity(dream);
-            message.channel.send(`✔️ **Done Playing Now __${dream}__**`)
-        }
-          if (message.content.startsWith(prefix + 'wt')) {
-      client.user.setActivity(dream, {type:'WATCHING'});
-          message.channel.send(`✔️  **Done Watching Now __${dream}__ **`)
-      } else
-      if (message.content.startsWith(prefix + 'ls')) {
-      client.user.setActivity(dream , {type:'LISTENING'});
-          message.channel.send(`✔️ **Done Listening Now __${dream}__** `)
-      } else
-      if (message.content.startsWith(prefix + 'st')) {
-    client.user.setGame(dream, "https://www.twitch.tv/dream");
-         message.channel.send(`✔️ **Done Stream Now  __${dream}__ **`)
-      }
- 
-    });
-
-
-client.login(process.env.LIGHT);
+client.login(process.env.BOT_TOKEN);
